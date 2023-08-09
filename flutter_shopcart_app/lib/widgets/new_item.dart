2fe3b6d48a947d5395c8773bc90b1f02168bd620
Elbert <elbertnatan@hotmail.com>
2023-08-09 10:16:25 -0300

    - salvando no firebase

--- flutter_shopcart_app/lib/widgets/new_item.dart
+++ flutter_shopcart_app/lib/widgets/new_item.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_shopcart_app/data/categories.dart';
 import 'package:flutter_shopcart_app/models/category.dart';
 import 'package:flutter_shopcart_app/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -21,6 +24,22 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     _formKey.currentState!.validate();
     _formKey.currentState!.save();
+    final url = Uri.https('flutter-backend-fe30c-default-rtdb.firebaseio.com',
+        'shopping-list.json');
+    http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode(
+        {
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _selectedCategory.title
+        },
+      ),
+    );
+
     Navigator.of(context).pop(GroceryItem(
         id: DateTime.now().toString(),
         name: _enteredName,
